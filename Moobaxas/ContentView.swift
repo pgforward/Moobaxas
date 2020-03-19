06caf007c0b26a802ffadd9304ab9e8623c59a14
Peter Forward <petejenny@hotmail.com>
2020-03-18 20:47:04 -0700

    Initial Commit

---
+++ Moobaxas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moobaxas
+//
+//  Created by Peter Forward on 3/18/20.
+//  Copyright © 2020 Peter Forward. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
