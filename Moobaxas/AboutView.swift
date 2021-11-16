092f7a9724342b740e19981fd05909db6d14dac8
Peter Forward <petejenny@hotmail.com>
2021-11-15 21:54:14 -0800

    Updated animation on About view

--- Moobaxas/AboutView.swift
+++ Moobaxas/AboutView.swift
@@ -23,17 +23,14 @@ func playSound(sound: String, type: String) {
 }
 
 struct AboutView: View {
-        
+    
     @Environment(\.presentationMode) var presentationMode
-    
-    @State var showAnimation = false
-    @State var rotation: Double = 0
-    
-    @State var scale: CGFloat = 0.8
-    @State var mult: CGFloat = 1
- 
+    let offsetConst: CGFloat = 0
 
-//    @State var offset: CGFloat = 1
+    @State var rotation: Double = 45
+    @State var scale: CGFloat = 1
+    @State var moo = true
+    @State private var allowAnimations : Bool = false
     
     var body: some View {
         NavigationView {
@@ -43,42 +40,64 @@ struct AboutView: View {
                     .frame(width: 241, height: 241)
                     .clipShape(RoundedRectangle(cornerRadius: 5.0))
                     .offset(x: 35)
+                    .animation(nil)
                 //                .padding(.bottom, 15)
                 HStack{
                     Spacer()
                     
                     VStack {
-                        
-                        Text("M")
-                            .padding(12)
-                            .background(Color.purple)
-                            .opacity(Double(scale))
-                            .scaleEffect(scale)
-                            .rotationEffect(.degrees(rotation))
-                            .onAppear {
-                                let baseAnimation = Animation.easeInOut(duration: 1).delay(3)
-                                let repeated = baseAnimation.repeatForever(autoreverses: true)
-                                return withAnimation(repeated) {
-                                    if self.rotation == 25 {
-                                        self.rotation = 315
-                                        self.mult = 0
-                                    } else {
-                                        self.rotation = 25
-                                        self.mult = 1
-                                    }
-                                    self.scale = 1
-                                }
-                        }
-
-                        CustomTextField(placeHolder: "O", rotation: rotation, scale: scale, offset: mult*10)
-                        CustomTextField(placeHolder: "O", rotation: rotation, scale: scale, offset: mult*20)
-                        CustomTextField(placeHolder: "B", rotation: rotation, scale: scale, offset: mult*30)
-                        CustomTextField(placeHolder: "A", rotation: rotation, scale: scale, offset: mult*40)
-                        CustomTextField(placeHolder: "X", rotation: rotation, scale: scale, offset: mult*50)
-                        CustomTextField(placeHolder: "A", rotation: rotation, scale: scale, offset: mult*60)
-                        CustomTextField(placeHolder: "S", rotation: rotation, scale: scale, offset: mult*70)
-
+                        CustomTextField(placeHolder: "M", rotation: rotation, scale: scale, offset: moo ? -40 : 65)
+                        CustomTextField(placeHolder: "O", rotation: rotation, scale: scale, offset: moo ? -20 : 50)
+                        CustomTextField(placeHolder: "O", rotation: rotation, scale: scale, offset: moo ? 0 : 35)
+                        CustomTextField(placeHolder: "B", rotation: rotation, scale: scale, offset: moo ? 20 : 20)
+                        CustomTextField(placeHolder: "A", rotation: rotation, scale: scale, offset: moo ? 40 : 5)
+                        CustomTextField(placeHolder: "X", rotation: rotation, scale: scale, offset: moo ? 60 : -10)
+                        CustomTextField(placeHolder: "A", rotation: rotation, scale: scale, offset: moo ? 80 : -25)
+                        CustomTextField(placeHolder: "S", rotation: rotation, scale: scale, offset: moo ? 100 : -40)
                     }
+                    .onAppear() {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            //                            self.allowAnimations = true
+                            let baseAnimation = Animation.easeInOut(duration: 1.5)
+                            let repeated = baseAnimation.repeatForever(autoreverses: true)
+                            withAnimation(repeated) {
+                                rotation = -45
+                                scale = 0.75
+                                moo = false
+                            }
+                        }
+                    }
+                    .onTapGesture {
+                        //                        .onAppear {
+                        let baseAnimation = Animation.easeInOut(duration: 1.5)
+                        let repeated = baseAnimation.repeatForever(autoreverses: true)
+                        withAnimation(repeated) {
+                            rotation = -45
+                            moo = false
+                        }
+                    }
+                    
+                    //                    .onAppear {
+                    //                        let baseAnimation = Animation.easeInOut(duration: 1)
+                    //                        let repeated = baseAnimation.repeatForever(autoreverses: true)
+                    //
+                    //                        withAnimation(repeated) {
+                    //                            scale = 0.5
+                    //                            mult = 0
+                    //                            rotation = 0
+                    //                        }
+                    //                    }
+                    
+                    //                    .onAppear {
+                    //                        let baseAnimation = Animation.easeInOut(duration: 1).delay(3)
+                    //                        let repeated = baseAnimation.repeatForever(autoreverses: true)
+                    //                        return withAnimation(repeated) {
+                    //                            rotation = 315
+                    //                            mult = 1
+                    //                            scale = 1
+                    //                        }
+                    //                    }
+                    
                     Spacer()
                     VStack {
                         Spacer()
@@ -86,6 +105,7 @@ struct AboutView: View {
                         Text("Created by Peter Forward 2020").font(.body)
                             .padding(.bottom, 35)
                     }
+                    .animation(nil)
                     Spacer()
                 }.font(.largeTitle)
                 Spacer()
@@ -98,10 +118,11 @@ struct AboutView: View {
         
         playSound(sound: "sheep.wav", type: "")
         
-        withAnimation {
-            self.rotation = 25
-            self.showAnimation.toggle();
-        }
+        //        withAnimation {
+        //            mult = 1
+        //            rotation = 25
+        //            showAnimation.toggle();
+        //        }
     }
     
     private var cancelButton: some View {
